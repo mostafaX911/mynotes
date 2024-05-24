e90c9bd8fd48c4ceb69095434903bc9b50ca90e1
Hamdi Standard AI <hamdi.mohamed@standard.ai>
2024-05-24 19:42:22 +0300

    update-3

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,9 @@
+import 'package:my_notes/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+}
