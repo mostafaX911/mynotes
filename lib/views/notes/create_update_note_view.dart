0ea08bcc897876c1440fbd97133d25cb37922fa3
Hamdi Standard AI <hamdi.mohamed@standard.ai>
2024-06-11 12:28:43 +0300

    changes-11-06-2024

---
+++ lib/views/notes/create_update_note_view.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:my_notes/services/auth/auth_service.dart';
+import 'package:my_notes/services/crud/notes_service.dart';
+import 'package:my_notes/utilities/generics/get_arguments.dart';
+
+class CreateUpdateNoteView extends StatefulWidget {
+  const CreateUpdateNoteView({super.key});
+
+  @override
+  State<CreateUpdateNoteView> createState() => _CreateUpdateNoteViewState();
+}
+
+class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
+  DatabaseNote? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  void _textControllerListener() async {
+    final note = _note;
+    if (note == null) {
+      return;
+    }
+    final text = _textController.text;
+    await _notesService.updateNote(
+      note: note,
+      text: text,
+    );
+  }
+
+  void _setupTextControllerListener() {
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+  }
+
+  Future<DatabaseNote> createOrGetExistingNote(BuildContext context) async {
+    final widgetNote = context.getArgument<DatabaseNote>();
+
+    if (widgetNote != null) {
+      _note = widgetNote;
+      _textController.text = widgetNote.text;
+      return widgetNote;
+    }
+
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    final newNote = await _notesService.createNote(owner: owner);
+    _note = newNote;
+    return newNote;
+  }
+
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    final note = _note;
+    final text = _textController.text;
+    if (text.isNotEmpty && note != null) {
+      await _notesService.updateNote(note: note, text: text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Note'),
+        backgroundColor: Colors.primaries[5],
+      ),
+      body: FutureBuilder(
+        future: createOrGetExistingNote(context),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              _setupTextControllerListener();
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _textController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your note here',
+                  ),
+                ),
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:my_notes/services/crud/notes_service.dart';
 import 'package:my_notes/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
 
   const NotesListView({
     super.key,
     required this.notes,
     required this.onDeleteNote,
+    required this.onTap,
   });
 
   @override
@@ -21,12 +23,15 @@ class NotesListView extends StatelessWidget {
         itemBuilder: (context, index) {
           final note = notes[index];
           return ListTile(
+            onTap: () => onTap(note),
             title: Text(
               note.text,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               softWrap: true,
-              style: const TextStyle(fontSize: 20),
+              style: const TextStyle(
+                fontSize: 18,
+              ),
             ),
             trailing: IconButton(
               onPressed: () async {
